f21dbbc171e785313da097b79319489c6fbd7f5e
SetterZets <107626440+SetterZets@users.noreply.github.com>
2022-09-27 23:00:23 +0300

    Add Main logic
    Add class Generator and WebSiteName
    Generator - main programm logic
    ChangeWebSiteName - Random Converter

---
+++ PasswordGenerator/PasswordGenerator/Model/ChangeWebSiteName.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordGenerator.Model
+{
+    public class ChangeWebSiteName
+    {
+        private int smallCharPercent;
+        private string webSiteName;
+        private char[] arrWebsiteName = new char[] { };
+
+
+        public string Converter(string name)
+        {
+            webSiteName = name;
+            ConverterToChar();
+            PercentageCalculation(arrWebsiteName.Length);
+            RandomHighChar();
+            return ConvertToString();
+        }
+
+        private string ConvertToString()
+        {
+            webSiteName = new string(arrWebsiteName);
+            return webSiteName;
+        }
+
+        private char[] ConverterToChar()
+        {
+            arrWebsiteName = webSiteName.ToCharArray();
+            return arrWebsiteName;
+        }
+
+
+        private void RandomHighChar()
+        {
+            Random random = new Random();
+            int RandomNumber;
+
+
+            for (int i = 0; i < smallCharPercent; ++i)
+            {
+                RandomNumber = random.Next(0, arrWebsiteName.Length);
+                arrWebsiteName[RandomNumber] = Char.ToUpper(arrWebsiteName[RandomNumber]);
+            }
+
+        }
+
+        private int PercentageCalculation(int numbers)
+        {
+            double result = numbers * 0.3;
+            smallCharPercent = Convert.ToInt32(result);
+            return smallCharPercent;
+        }
+    }
+}
---
+++ PasswordGenerator/PasswordGenerator/Model/Generator.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordGenerator.Model
+{
+    public class Generator
+    {
+
+        ChangeWebSiteName changeWebSiteName = new ChangeWebSiteName();
+        string [] RandomFillingOfBlocks;
+        int KeyLenght;
+        int NumberOfBlocks;
+        string WebSiteName;
+        string iPass = "";
+        string[] arr = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "B",
+                            "C", "D", "F", "G", "H", "J", "K", "L", "M", "N",
+                            "P", "Q", "R", "S", "T", "V", "W", "X", "Z", "b",
+                            "c", "d", "f", "g", "h", "j", "k", "m", "n", "p",
+                            "q", "r", "s", "t", "v", "w", "x", "z", "A", "E",
+                            "U", "Y", "a", "e", "i", "o", "u", "y" };
+
+        
+
+        string[] BAZA()
+        {
+            WebSiteName = changeWebSiteName.Converter(WebSiteName);
+            RandomFillingOfBlocks = new string[NumberOfBlocks];
+        }
+
+        void NameBlender()
+        {
+
+            Random random = new Random();
+
+            int RandomChoose = random.Next(0, NumberOfBlocks);
+
+
+            //алгоритм случайным образом впихивающий переменную WebSiteName
+            for (int l = RandomChoose - 1; l < RandomChoose; l++)
+            {
+                RandomFillingOfBlocks[RandomChoose] = WebSiteName;
+            }
+
+        }
+
+
+        //через цикл фор выводим значения массива после блендинга
+        void OutputValueBlender()
+        {
+
+            for (int i = 0; i < RandomFillingOfBlocks.Length; i++)
+            {
+                Console.Write(RandomFillingOfBlocks[i]);
+            }
+        }
+
+
+        //получаем название сайта
+        void GetWebSiteName()
+        {
+            Console.WriteLine("Введите назвение Website'а ");
+            WebSiteName = Console.ReadLine();
+
+        }
+
+        void GetKeyLenght()
+        {
+            Console.WriteLine("Введите необходимое колличество символов вашего пароля");
+            KeyLenght = Convert.ToInt32(Console.ReadLine());
+        }
+
+        void GetBlocksLenght()
+        {
+            Console.WriteLine("Введите необходимое колличество блоков вашего пароля");
+            NumberOfBlocks = Convert.ToInt32(Console.ReadLine());
+        }
+
+
+
+        //генерируем случайные символы для блока пароля
+        void GeneratePass()
+        {
+            Random rnd = new Random();
+            for (int i = 0; i <= NumberOfBlocks - 1; i++)
+            {
+                for (int j = 0; j < KeyLenght; j++)
+                {
+                    iPass = iPass + arr[rnd.Next(0, arr.Length)];
+
+                }
+                RandomFillingOfBlocks[i] = iPass;
+                iPass = null;
+            }
+
+
+
+        }
+
+    }
+}
